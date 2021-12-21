68ab8787f78a6a311b1e65e1f4eed658c2b97fec
Zé Lúcio <joselucioalmeidajunior@gmail.com>
2021-12-21 02:47:40 -0300

    feat: adding handler for NullIdException

--- src/main/java/com/julucin/hrworkers/exceptions/handler/ApiExceptionHandler.java
+++ src/main/java/com/julucin/hrworkers/exceptions/handler/ApiExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.julucin.hrworkers.exceptions.handler;
 
 import com.julucin.hrworkers.exceptions.InstanceDoesNotExistException;
+import com.julucin.hrworkers.exceptions.NullIdException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,6 +12,11 @@ public class ApiExceptionHandler {
     @ExceptionHandler(value = InstanceDoesNotExistException.class)
     public ResponseEntity<ExceptionHandlerResponse> handle(InstanceDoesNotExistException exception){
         return ResponseEntity.status(404).body(ExceptionHandlerResponse.of(exception));
+    }
+
+    @ExceptionHandler(value = NullIdException.class)
+    public ResponseEntity<ExceptionHandlerResponse> handle(NullIdException exception){
+        return ResponseEntity.status(400).body(ExceptionHandlerResponse.of(exception));
     }
 
 }
