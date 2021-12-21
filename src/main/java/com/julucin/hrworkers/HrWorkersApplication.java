774057449a20a41ec893401444bda3d88e2edb04
Zé Lúcio <joselucioalmeidajunior@gmail.com>
2021-12-21 00:22:53 -0300

    feat: starting project

---
+++ src/main/java/com/julucin/hrworkers/HrWorkersApplication.java
@@ -0,0 +1,13 @@
+package com.julucin.hrworkers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorkersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/julucin/hrworkers/HrWorkersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julucin.hrworkers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
